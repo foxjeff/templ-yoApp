bff8d585f2451593178ed2bb0a9436bb67b1ed18
Jeff Fox <foxjeff@pobox.com>
2014-04-27 13:50:29 -0500

    navigation working better

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,9 +7,18 @@ angular
     'ngSanitize',
     'ngRoute'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    // if this is set, take the "#" out of the paths in index.html
+    // $locationProvider.html5Mode(true);
+
     $routeProvider
       .when('/', {
+        template: '<h1>DoIt</h1>'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html'
+      })
+      .when('/yeoman', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
